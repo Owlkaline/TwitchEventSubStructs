417b82c17044849222c02c66a96c5f3e78e06ff3
Owlkaline <lilith@inet-sys.com>
2024-07-22 00:18:28 +1000

    reexport all serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+pub use serde;
+pub use serde_derive;
 pub use serde_derive::{Deserialize as Deserialise, Serialize as Serialise};
 pub use serde_json;
+pub use serde_with;
 
 mod response_messages;
 mod solid_structs;
