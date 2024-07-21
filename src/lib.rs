91dda2ce67b54470d348515949ed045785a1d561
Owlkaline <lilith@inet-sys.com>
2024-07-22 01:14:06 +1000

    removed serde derive from reexport

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub use serde;
-pub use serde_derive;
-pub use serde_derive::{Deserialize as Deserialise, Serialize as Serialise};
+use serde_derive::{Deserialize as Deserialise, Serialize as Serialise};
 pub use serde_json;
 pub use serde_with;
 
